cd1783db1a2d85d04449ba2cf535b43bd3eb1dd6
Kenshi Kawaguchi <kenshi@recurse.ca>
2017-02-02 20:47:48 -0800

    initial client class

---
+++ src/client.ts
@@ -0,0 +1,76 @@
+import {EventEmitter} from 'events'
+
+interface LibratoClientConfig {
+  email: string,
+  token: string,
+  prefix?: string,
+  period?: number,
+}
+
+interface IncrementOptions {
+  amount?: number,
+  sporadic?: true,
+  source?: string
+  tags?: Object
+}
+
+export class LibratoClient extends EventEmitter {
+  config: LibratoClientConfig
+
+  protected counters : Object
+  protected interval : NodeJS.Timer
+  protected period : number
+  protected sporadics : Set<string>
+
+  constructor(config : LibratoClientConfig) {
+    super()
+    this.config = config
+    this.period = config.period || 60000
+    this.counters = {}
+    this.sporadics = new Set()
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      this.submitMetrics()
+    }, this.config.period)
+  }
+
+  submitMetrics() {
+    this.submitCounters()
+    this.resetCounters()
+  }
+
+  submitCounters() {
+    console.log('flushing', this.counters)
+  }
+
+  resetCounters() {
+    for(let metric in this.sporadics.entries()) {
+      delete this.counters[metric]
+    }
+    this.sporadics = new Set()
+
+    for(let metric in this.counters) {
+      this.counters[metric] = 0
+    }
+  }
+
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  /*
+    For reference: https://github.com/librato/librato-rack/blob/master/lib/librato/collector/counter_cache.rb
+  */
+  increment(metric : string, options? : IncrementOptions) {
+    const amount = options.amount || 1
+    const metricName = (this.config.prefix || '') + metric
+    const newCount = (this.counters[metricName] || 0) + amount
+    if(options.sporadic) {
+      this.sporadics.add(metricName)
+    }
+    this.counters[metricName] = newCount
+  }
+
+}
