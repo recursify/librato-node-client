31a9027c063d09527918e621d6a66a251dbb3359
Kenshi Kawaguchi <kenshi@recurse.ca>
2017-06-15 14:16:24 -0700

    copy types

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,13 @@ var ts = require('gulp-typescript')
 var jasmine = require('gulp-jasmine')
 var SpecReporter  = require('jasmine-spec-reporter')
 
-gulp.task('default', ['typescript'])
+gulp.task('default', ['typescript', 'copy-types'])
+
+gulp.task('copy-types', function () {
+  gulp.src('./src/types.d.ts')
+    .pipe(gulp.dest('./dist'));
+});
+
 
 gulp.task('typescript', function() {
   var tsProject = ts.createProject("./src/tsconfig.json")
